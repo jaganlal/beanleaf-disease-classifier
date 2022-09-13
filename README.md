a7c395f39c21ae6c0d148975581c26762ca5c86e
jaganlal@gmail.com <jaganlal@gmail.com>
2022-09-13 00:21:32 -0400

    updated dependencies

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # beanleaf-disease-classifier
-Simple MLOps demo for classifying bean leaf diseases
+MLOps demo for classifying bean leaf diseases
--- environment_setup/install-requirements.sh
+++ environment_setup/install-requirements.sh
@@ -4,3 +4,7 @@ pip install azure-cli
 pip install --upgrade azureml-sdk[cli]
 pip install pytest
 pip install pytest-cov
+pip install tensorflow
+pip install tensorflow_hub
+pip install matplotlib
+pip install numpy
