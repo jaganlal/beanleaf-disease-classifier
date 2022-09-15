dbb44753988749094a2952cb004212a3e4b614b8
jaganlal@gmail.com <jaganlal@gmail.com>
2022-09-14 21:12:34 -0400

    requests.post data instead of json

--- tests/smoke/smoke_tests.py
+++ tests/smoke/smoke_tests.py
@@ -2,13 +2,13 @@ import requests
 import json
 import logging
 
-req_sample = {"path": "https://beanipm.pbgworks.org/sites/pbg-beanipm7/files/styles/picture_custom_user_wide_1x/public/AngularLeafSpotFig1a.jpg"}
+req_sample = {'path': 'https://beanipm.pbgworks.org/sites/pbg-beanipm7/files/styles/picture_custom_user_wide_1x/public/AngularLeafSpotFig1a.jpg'}
 classes = ['angular_leaf_spot','bean_rust','healthy']
 
 def test_ml_service(scoreurl):
     assert scoreurl != None
     headers = {'Content-Type':'application/json'}
-    resp = requests.post(scoreurl, json=json.loads(json.dumps(req_sample)), headers=headers)
+    resp = requests.post(scoreurl, data=json.loads(json.dumps(req_sample)), headers=headers)
     assert resp.status_code == requests.codes['ok']
     assert resp.text != None
     assert resp.headers.get('content-type') == 'application/json'
@@ -17,7 +17,7 @@ def test_ml_service(scoreurl):
 def test_prediction(scoreurl):
     assert scoreurl != None
     headers = {'Content-Type':'application/json'}
-    resp = requests.post(scoreurl, json=json.loads(json.dumps(req_sample)), headers=headers)
+    resp = requests.post(scoreurl, data=json.loads(json.dumps(req_sample)), headers=headers)
     resp_json = json.loads(resp.text)
-    assert resp_json['output']['predicted_class'] == 'bean_rust'
+    assert resp_json['output']['predicted_class'] == 'angular_leaf_spot'
     logging.info(resp)
