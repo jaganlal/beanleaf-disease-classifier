670777ee16a0995a7614daabf93e1852895a5eac
jaganlal@gmail.com <jaganlal@gmail.com>
2022-09-14 22:20:26 -0400

    set log level to info

--- tests/smoke/smoke_tests.py
+++ tests/smoke/smoke_tests.py
@@ -3,7 +3,7 @@ import json
 import logging
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 req_sample = {'path': 'https://beanipm.pbgworks.org/sites/pbg-beanipm7/files/styles/picture_custom_user_wide_1x/public/AngularLeafSpotFig1a.jpg'}
 classes = ['angular_leaf_spot','bean_rust','healthy']
@@ -15,6 +15,7 @@ def test_ml_service(scoreurl):
     logger.info('Test ml service request:', json.dumps(req_sample))
     resp = requests.post(scoreurl, json=prediction_request, headers=headers)
     logger.info('ML service response:', resp.status_code)
+    logger.debug('ML service response - DEBUG:', resp.status_code)
     assert resp.status_code == requests.codes['ok']
     assert resp.text != None
     assert resp.headers.get('content-type') == 'application/json'
